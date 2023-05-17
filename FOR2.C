385cce6b262bf760b0e0e510928ae79805b22524
vrundpadariya <vrundpadariya725@gmail.com>
2023-05-17 12:44:49 +0530

    first time

---
+++ FOR2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int a;
+int b;
+int c;
+clrscr();
+printf("enter the value of a ");
+scanf("%d",&a);
+
+printf("enter the value of b");
+scanf("%d",&b);
+
+c=(a-b)*2;
+
+printf("the answer of c is %d",c);
+getch();
+}
